d63f49ad3d995772732b5142dd3665a185f8a43e
Charalampos Vitoros (Χαράλαμπος Βιτώρος) <charalampos.vitoros@sitecore.com>
2025-05-16 16:55:58 +0300

    Demo page + reading colors.css to extract values

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,6 @@
+import fs from "fs"
+import path from "path"
+
 import { AccordionDemo } from "@/components/accordion-demo"
 import { AlertDemo } from "@/components/alert-demo"
 import { AlertDialogDemo } from "@/components/alert-dialog-demo"
@@ -12,6 +15,7 @@ import { CarouselDemo } from "@/components/carousel-demo"
 import { ChartDemo } from "@/components/chart-demo"
 import { CheckboxDemo } from "@/components/checkbox-demo"
 import { CollapsibleDemo } from "@/components/collapsible-demo"
+import ColorsDemo from "@/components/colors-demo"
 import { ComboboxDemo } from "@/components/combobox-demo"
 import { CommandDemo } from "@/components/command-demo"
 import { ComponentWrapper } from "@/components/component-wrapper"
@@ -46,6 +50,9 @@ import { TextareaDemo } from "@/components/textarea-demo"
 import { ToggleDemo } from "@/components/toggle-demo"
 import { ToggleGroupDemo } from "@/components/toggle-group-demo"
 import { TooltipDemo } from "@/components/tooltip-demo"
+
+const cssPath = path.join(process.cwd(), "app", "colors.css")
+const cssContent = fs.readFileSync(cssPath, "utf-8")
 
 export default function SinkPage() {
   return (
@@ -190,6 +197,9 @@ export default function SinkPage() {
       </ComponentWrapper>
       <ComponentWrapper name="tooltip">
         <TooltipDemo />
+      </ComponentWrapper>
+      <ComponentWrapper name="colors">
+        <ColorsDemo cssContent={cssContent} />
       </ComponentWrapper>
     </div>
   )
