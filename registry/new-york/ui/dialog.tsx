767c1ef7ba6fc42332bbb0f6aa3e3e7b1f2b5046
Thomas Kelly <thomaskelly281@gmail.com>
2025-05-29 11:02:16 +0100

    Update dialog.tsx

--- registry/new-york/ui/dialog.tsx
+++ registry/new-york/ui/dialog.tsx
@@ -65,10 +65,7 @@ function DialogContent({
         {...props}
       >
         {children}
-        {/* <DialogPrimitive.Close className="ring-offset-background focus:ring-ring data-[state=open]:bg-accent data-[state=open]:text-muted-foreground absolute top-4 right-4 rounded-xs opacity-70 transition-opacity hover:opacity-100 focus:ring-2 focus:ring-offset-2 focus:outline-hidden disabled:pointer-events-none [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4">
-          <Icon path={mdiClose} size={0.9} />
-          <span className="sr-only">Close</span>
-        </DialogPrimitive.Close> */}
+       
         <DialogPrimitive.Close
           className={cn(
             buttonVariants({ variant: "ghost", colorScheme: "neutral", size: "icon" }),
