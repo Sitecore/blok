ad5a4caac44e5c9aa187042c6ca1aafd481dd7d6
Vishath Amarasinghe <vishath.amarasinghe@igt1sl.com>
2025-08-07 08:57:48 +0530

    configuration file added

---
+++ config/links.ts
@@ -0,0 +1,5 @@
+export const externalLinks = {
+  Block_github: "https://github.com/Sitecore/blok",
+  Block_site: "https://blok.sitecore.com",
+  Block_Logo:"https://delivery-sitecore.sitecorecontenthub.cloud/api/public/content/logo-blok"
+}
