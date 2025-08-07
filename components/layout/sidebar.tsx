cf5df53d6acc32f4b9284931e73f7321a7b51286
Muthula <muhtula.dev@gmail.com>
2025-08-07 13:29:06 +0530

    Added core sidebar component

---
+++ components/layout/sidebar.tsx
@@ -0,0 +1,43 @@
+'use client'
+
+import { cn } from "@/registry/new-york/lib/utils";
+import { Sidebar, SidebarContent, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from "@/registry/new-york/ui/sidebar";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+const items = [
+  { label: "Item 1", href: "/i1" },
+  { label: "Item 2", href: "/i2" },
+  { label: "Item 3", href: "/i3" },
+];
+
+export default function AppSidebar() {
+    const pathname = usePathname();
+
+    return (
+        <Sidebar className="w-[200px] bg-[#f7f7f7] py-6 px-2">
+            <SidebarContent className="flex-1 overflow-y-auto">
+                <SidebarMenu className="space-y-2">
+                    {items.map((item) => {
+                        const isActive = pathname === item.href;
+
+                        return (
+                            <SidebarMenuItem key={item.href}>
+                                <SidebarMenuButton 
+                                    className={cn(
+                                        "rounded-md px-2.5",
+                                        "hover:bg-primary-background hover:text-primary-fg active:bg-primary-background active:text-primary-fg",
+                                        isActive && "bg-primary-background text-primary-fg"
+                                    )}
+                                    asChild
+                                >
+                                    <Link href={item.href} className="text-md font-medium">{item.label}</Link>
+                                </SidebarMenuButton>
+                            </SidebarMenuItem>
+                        );
+                    })}
+                </SidebarMenu>
+            </SidebarContent>
+        </Sidebar>
+    )
+}
