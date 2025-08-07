a9637fa55b0ce9fa7b7d0d8960e601ff11cd3762
Muthula <muhtula.dev@gmail.com>
2025-08-07 15:05:28 +0530

    Updated layout to handle proper scrolling

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <SidebarProvider className="w-full h-screen flex flex-col">
+        <SidebarProvider className="w-full h-screen flex flex-col overflow-y-hidden">
           <header className="bg-background sticky inset-x-0 top-0 isolate z-10 flex shrink-0 items-center gap-2 border-b">
             <div className="flex h-14 w-full items-center gap-2 px-4">
               <TopBar />
@@ -40,7 +40,7 @@ export default function RootLayout({
           </header>
           <div className="flex flex-1 h-full">
             <Sidebar />
-            <main className="flex-1">
+            <main className="flex-1 overflow-y-auto">
               {children}
             </main>
           </div>
