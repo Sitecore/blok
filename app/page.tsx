040fa54d7e9fbd6be2ad155b5a06373ab34eddd0
sc-ishankalakshan <sc.ishanka@gmail.com>
2025-08-07 12:55:20 +0530

    Home page hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from "react"
 
-const page = () => {
+import { HeroSection } from "@/components/heroSection"
+
+export default function page() {
+  const buttons = [
+    {
+      text: "Get started",
+      variant: "default",
+      className:
+        "rounded-full bg-purple-500 text-white hover:bg-purple-700 text-xs w-30",
+    },
+    {
+      text: "Browse blocks",
+      variant: "outline",
+      className:
+        "rounded-full border-gray-300 text-gray-600 hover:bg-gray-100 text-xs w-30",
+    },
+  ]
+
   return (
-    <div>Home page</div>
+    <div>
+      <div>
+        <HeroSection
+          title="Build better products faster"
+          description="Blok is Sitecore's product design system: the UI framework and style guide we use to build great apps. It's publicly available, so that anyone can easily build software in the Sitecore product design language."
+          buttons={buttons}
+        />
+      </div>
+    </div>
   )
 }
-
-export default page
