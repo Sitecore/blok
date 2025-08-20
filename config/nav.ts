087d35850929e0c1770cc9b40c3deeae0c7e808d
Vishath Amarasinghe <vishath.amarasinghe@igt1sl.com>
2025-08-20 13:06:48 +0530

    typo error fixed

--- config/nav.ts
+++ config/nav.ts
@@ -1,7 +1,7 @@
 // navigation items for the site
 export const navItems = [
   { name: "Components", href: "/components" },
-  { name: "Blocks", href: "/blocks" },
+  { name: "Bloks", href: "/blocks" },
   { name: "Theming", href: "/theming" },
   { name: "Graphics", href: "/graphics" },
   { name: "Resources", href: "/resources" },
