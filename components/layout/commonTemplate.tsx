73b122eef091a27a05efb01c003508648801bf3c
sc-ArshadHannan <arsh@sitecore.net>
2025-08-21 13:55:11 +0530

    heading and exmple title bug fix

--- components/layout/commonTemplate.tsx
+++ components/layout/commonTemplate.tsx
@@ -72,10 +72,11 @@ export const CommonTemplate = <T extends object>({
         </div>
 
         <div className="space-y-4">
-          <h2 className="text-2xl md:text-3xl font-semibold" id="examples">Examples</h2>
-          <p>The following is examples of our {pageTitle} class </p>
+
           {page === "component" && Array.isArray(config?.demos) && config.demos.length > 0 && (
             <>
+              <h2 className="text-2xl md:text-3xl font-semibold" id="examples">Examples</h2>
+              <p>The following is examples of our {pageTitle} class </p>
               {
                 config.demos?.map((demo, index) => (
                   <React.Fragment key={index}>
@@ -87,6 +88,8 @@ export const CommonTemplate = <T extends object>({
           )}
           {page === "blok" && (
             <div>
+              <h2 className="text-2xl md:text-3xl font-semibold" id="examples">Examples</h2>
+              <p>The following is examples of our {pageTitle} class </p>
               {React.cloneElement(children, {
                 ...config,
                 selectedDemo: config?.mainDemo
