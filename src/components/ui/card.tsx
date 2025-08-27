23fe0380bc507ddffccd0e3510b275663b5ae513
Vishath Amarasinghe <vishath.amarasinghe@igt1sl.com>
2025-08-27 13:15:45 +0530

    card component registry and demo pages implemented successfully.

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -1,18 +1,57 @@
-import type * as React from "react";
+import * as React from "react"
+import { cva, type VariantProps } from "class-variance-authority"
 
-import { cn } from "@/lib/utils";
+import { cn } from "@/lib/utils"
 
-function Card({ className, ...props }: React.ComponentProps<"div">) {
+const cardVariants = cva(
+  "flex flex-col gap-6 rounded-xl border transition-shadow",
+  {
+    variants: {
+      elevation: {
+        none: "shadow-none",
+        xs: "shadow-xs",
+        sm: "shadow-sm",
+        base: "shadow-base",
+        md: "shadow-md",
+        lg: "shadow-lg",
+        // xl: "shadow-xl",
+        // outline: "shadow-outline",
+        // inner: "shadow-inner",
+        // darkLg: "shadow-dark-lg",
+      },
+      style: {
+        flat: "bg-white border-transparent",
+        outline: "bg-white border-color-border",
+        filled: "bg-neutral-50 border-transparent",
+      },
+      padding: {
+        sm: "px-3 py-3 rounded-base",
+        md: "px-5 py-5 rounded-md",
+        lg: "px-7 py-7 rounded-lg",
+      },
+    },
+    defaultVariants: {
+      elevation: "sm",
+      style: "flat",
+      padding: "lg",
+    },
+  }
+)
+
+function Card({
+  className,
+  elevation,
+  style,
+  padding,
+  ...props
+}: React.ComponentProps<"div"> & VariantProps<typeof cardVariants>) {
   return (
     <div
       data-slot="card"
-      className={cn(
-        "flex flex-col gap-6 rounded-xl border bg-card py-6 text-card-foreground shadow-sm",
-        className,
-      )}
+      className={cn(cardVariants({ elevation, style, padding }), className)}
       {...props}
     />
-  );
+  )
 }
 
 function CardHeader({ className, ...props }: React.ComponentProps<"div">) {
@@ -20,22 +59,22 @@ function CardHeader({ className, ...props }: React.ComponentProps<"div">) {
     <div
       data-slot="card-header"
       className={cn(
-        "@container/card-header grid auto-rows-min grid-rows-[auto_auto] items-start gap-1.5 px-6 has-data-[slot=card-action]:grid-cols-[1fr_auto] [.border-b]:pb-6",
-        className,
+        "@container/card-header grid auto-rows-min grid-rows-[auto_auto] items-start gap-1.5 has-data-[slot=card-action]:grid-cols-[1fr_auto] [.border-b]:pb-6",
+        className
       )}
       {...props}
     />
-  );
+  )
 }
 
 function CardTitle({ className, ...props }: React.ComponentProps<"div">) {
   return (
     <div
       data-slot="card-title"
-      className={cn("font-semibold leading-none", className)}
+      className={cn("leading-none font-semibold", className)}
       {...props}
     />
-  );
+  )
 }
 
 function CardDescription({ className, ...props }: React.ComponentProps<"div">) {
@@ -45,7 +84,7 @@ function CardDescription({ className, ...props }: React.ComponentProps<"div">) {
       className={cn("text-muted-foreground text-sm", className)}
       {...props}
     />
-  );
+  )
 }
 
 function CardAction({ className, ...props }: React.ComponentProps<"div">) {
@@ -54,31 +93,27 @@ function CardAction({ className, ...props }: React.ComponentProps<"div">) {
       data-slot="card-action"
       className={cn(
         "col-start-2 row-span-2 row-start-1 self-start justify-self-end",
-        className,
+        className
       )}
       {...props}
     />
-  );
+  )
 }
 
 function CardContent({ className, ...props }: React.ComponentProps<"div">) {
   return (
-    <div
-      data-slot="card-content"
-      className={cn("px-6", className)}
-      {...props}
-    />
-  );
+    <div data-slot="card-content" className={cn("", className)} {...props} />
+  )
 }
 
 function CardFooter({ className, ...props }: React.ComponentProps<"div">) {
   return (
     <div
       data-slot="card-footer"
-      className={cn("flex items-center px-6 [.border-t]:pt-6", className)}
+      className={cn("flex items-center [.border-t]:pt-6", className)}
       {...props}
     />
-  );
+  )
 }
 
 export {
@@ -89,4 +124,4 @@ export {
   CardAction,
   CardDescription,
   CardContent,
-};
+}
