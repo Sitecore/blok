24e3c046899be120a30b278d2f4646857a861853
Muthula Alwis <215100981+Sc-MuthulaAlwis@users.noreply.github.com>
2025-08-27 12:54:08 +0530

    Integration of the button group component to the doc site (#69)
    * Imported new button component
    
    * Updated non existing button variants with existing variants
    
    ---------
    
    Co-authored-by: Muthula <muhtula.dev@gmail.com>

--- src/app/demo/[name]/ui/button.tsx
+++ src/app/demo/[name]/ui/button.tsx
@@ -4,7 +4,8 @@ export const button = {
   name: "button",
   components: {
     Primary: <Button variant="default">Primary</Button>,
-    Secondary: <Button variant="secondary">Secondary</Button>,
     Outline: <Button variant="outline">Outline</Button>,
+    Ghost: <Button variant="ghost">Ghost</Button>,
+    Link: <Button variant="link">Link</Button>,
   },
 };
