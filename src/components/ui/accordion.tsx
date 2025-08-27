174ccaf55afed5ec00697b1b5c73bef00fc4e3f9
Muthula <muhtula.dev@gmail.com>
2025-08-27 16:38:51 +0530

    Added lint fixes for accordion component

--- src/components/ui/accordion.tsx
+++ src/components/ui/accordion.tsx
@@ -1,16 +1,16 @@
-"use client"
+"use client";
 
-import * as React from "react"
-import { mdiChevronDown } from "@mdi/js"
-import Icon from "@mdi/react"
-import * as AccordionPrimitive from "@radix-ui/react-accordion"
+import { mdiChevronDown } from "@mdi/js";
+import Icon from "@mdi/react";
+import * as AccordionPrimitive from "@radix-ui/react-accordion";
+import type * as React from "react";
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
 
 function Accordion({
   ...props
 }: React.ComponentProps<typeof AccordionPrimitive.Root>) {
-  return <AccordionPrimitive.Root data-slot="accordion" {...props} />
+  return <AccordionPrimitive.Root data-slot="accordion" {...props} />;
 }
 
 function AccordionItem({
@@ -23,7 +23,7 @@ function AccordionItem({
       className={cn("border-b last:border-b-0", className)}
       {...props}
     />
-  )
+  );
 }
 
 function AccordionTrigger({
@@ -37,7 +37,7 @@ function AccordionTrigger({
         data-slot="accordion-trigger"
         className={cn(
           "focus-visible:border-ring focus-visible:ring-ring/50 text-md font-regular hover:bg-blackAlpha-50 flex flex-1 cursor-pointer items-start justify-between gap-4 px-2 py-4 text-left transition-all outline-none focus-visible:ring-[3px] disabled:pointer-events-none disabled:opacity-50 [&[data-state=open]>svg]:rotate-180",
-          className
+          className,
         )}
         {...props}
       >
@@ -49,7 +49,7 @@ function AccordionTrigger({
         />
       </AccordionPrimitive.Trigger>
     </AccordionPrimitive.Header>
-  )
+  );
 }
 
 function AccordionContent({
@@ -62,13 +62,13 @@ function AccordionContent({
       data-slot="accordion-content"
       className={cn(
         "data-[state=closed]:animate-accordion-up data-[state=open]:animate-accordion-down text-md overflow-hidden px-2 py-2",
-        className
+        className,
       )}
       {...props}
     >
       <div className={cn("pt-0 pb-4", className)}>{children}</div>
     </AccordionPrimitive.Content>
-  )
+  );
 }
 
-export { Accordion, AccordionItem, AccordionTrigger, AccordionContent }
+export { Accordion, AccordionItem, AccordionTrigger, AccordionContent };
