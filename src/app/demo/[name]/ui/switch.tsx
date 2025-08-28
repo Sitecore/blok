6cef941d3305a0e588eb1199933455c8a1097bf4
Vishath Amarasinghe <vishath.amarasinghe@igt1sl.com>
2025-08-28 09:49:53 +0530

    demo and registry components implemented for switch

--- src/app/demo/[name]/ui/switch.tsx
+++ src/app/demo/[name]/ui/switch.tsx
@@ -4,11 +4,44 @@ import { Switch } from "@/components/ui/switch";
 export const switchComponent = {
   name: "switch",
   components: {
-    Default: (
-      <div className="flex items-center space-x-2">
-        <Switch id="airplane-mode" />
-        <Label htmlFor="airplane-mode">Airplane Mode</Label>
+    Primary: (
+      <div className="flex items-center gap-2">
+        <Switch id="switch-demo-airplane-mode" variant="primary" />
+        <Label htmlFor="switch-demo-airplane-mode">Airplane Mode</Label>
       </div>
+    ),
+    Danger: (
+      <div className="flex items-center gap-2">
+        <Switch
+          id="switch-demo-danger"
+          variant="danger"
+        />
+        <Label htmlFor="switch-demo-danger">Danger Mode</Label>
+      </div>
+    ),
+    Success: (
+      <div className="flex items-center gap-2">
+        <Switch
+          id="switch-demo-success"
+          variant="success"
+        />
+        <Label htmlFor="switch-demo-success">Success Mode</Label>
+      </div>
+    ),
+    FocusMode: (
+      <Label className="flex items-center gap-6 rounded-lg border p-4 has-[[data-state=checked]]:border-info-600">
+        <div className="flex flex-col gap-1">
+          <div className="font-medium">Share across devices</div>
+          <div className="text-muted-foreground text-sm font-normal">
+            Focus is shared across devices, and turns off when you leave the
+            app.
+          </div>
+        </div>
+        <Switch
+          id="switch-demo-focus-mode"
+          variant="primary"
+        />
+      </Label>
     ),
   },
 };
