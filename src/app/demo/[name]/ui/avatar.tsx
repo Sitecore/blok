e181e6800c833ee543e86ae747ab400c9890544a
Vishath Amarasinghe <vishath.amarasinghe@igt1sl.com>
2025-08-28 09:24:12 +0530

    linter issues fixed

--- src/app/demo/[name]/ui/avatar.tsx
+++ src/app/demo/[name]/ui/avatar.tsx
@@ -40,7 +40,8 @@ export const avatar = {
       </div>
     ),
     Interactive: (
-      <div className="*:data-[slot=avatar]:ring-background flex -space-x-2 hover:space-x-1 *:data-[slot=avatar]:size-12 *:data-[slot=avatar]:ring-2 *:data-[slot=avatar]:grayscale 
+      <div className="*:data-[slot=avatar]:ring-background flex -space-x-2 hover:space-x-1 
+        *:data-[slot=avatar]:size-12 *:data-[slot=avatar]:ring-2 *:data-[slot=avatar]:grayscale 
         *:data-[slot=avatar]:transition-all *:data-[slot=avatar]:duration-300 *:data-[slot=avatar]:ease-in-out">
         <Avatar>
           <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
