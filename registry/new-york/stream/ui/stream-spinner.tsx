033eadb723cd2a23595d876e6422d6f13e3b2414
Antonios Liakakis <liak@sitecore.net>
2025-09-03 11:41:35 +0300

    Fixed imports

--- registry/new-york/stream/ui/stream-spinner.tsx
+++ registry/new-york/stream/ui/stream-spinner.tsx
@@ -1,6 +1,6 @@
 import React from "react"
 
-import { cn } from "@/registry/new-york/lib/utils"
+import { cn } from "../lib/utils"
 
 interface StreamSpinnerProps extends React.HTMLAttributes<HTMLDivElement> {
   size?: "xs" | "sm" | "md" | "lg" | "xl"
