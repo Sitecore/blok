6a2fa0ec9babc72c1f0ea52b6979d4b32d132dbd
Ishanka Lakshan <sc.ishanka@gmail.com>
2025-09-05 15:46:54 +0530

    Registry home page enhancement (#118)
    * Integration of the input component to the doc site
    
    * Integration of the input component to the doc site revert
    
    * Registry home page enhancement

--- src/app/(registry)/page.tsx
+++ src/app/(registry)/page.tsx
@@ -1,163 +1,66 @@
-import { ArrowRight, Blocks, Component, ToyBrick } from "lucide-react";
-import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import { Codeblocks } from "@/components/registry/code-block";
 
-import { MCPTabs } from "@/components/registry/mcp-tabs";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import { getBlocks, getComponents } from "@/lib/registry";
+const InstallationCode = `npx shadcn@latest add ${process.env.VERCEL_PROJECT_PRODUCTION_URL}/r/button.json `;
 
-const componentItems = getComponents().slice(0, 5);
-const blockItems = getBlocks().slice(0, 5);
+const sampleButtonCode = `import { Button } from "@/components/ui/button"
+export function ButtonDemo() {
+  return (
+    <div className="flex flex-wrap items-center gap-2 md:flex-row">
+      <Button>Button</Button>
+    </div>
+  )
+}`;
 
 export default function Home() {
   return (
-    <main className="container mt-4 p-5 md:mt-8 md:p-10">
-      <div className="mb-8">
-        <div className="space-y-2">
-          <h1 className="font-bold text-3xl tracking-tight md:text-4xl">
-            Registry
+    <main className="w-full">
+      <div className="bg-white p-6 ">
+        <div className="flex flex-col space-y-5 mt-4 p-5 md:mt-8 md:p-10">
+          <h1 className="font-bold text-4xl tracking-tight md:text-4xl">
+            Build better products faster
           </h1>
-          <p className="text-muted-foreground">
-            Distribute your design system tokens, custom components, hooks,
-            pages, and other files to any React project.
+          <p className="text-muted-foreground w-full ">
+            Blok is Sitecore's product design system: the UI framework and style
+            guide we use to build great apps. It's publicly available, so that
+            anyone can easily build software in the Sitecore product design
+            language.
           </p>
+          <div className="flex space-x-4">
+            <Button className="bg-primary text-white px-4 py-2 rounded-full text-xs">
+              Get started
+            </Button>
+            <Button
+              variant="outline"
+              className="rounded-full border-gray-300 text-gray-600 hover:bg-gray-100 text-xs w-30"
+            >
+              Browse blocks
+            </Button>
+          </div>
         </div>
       </div>
-
-      <Card className="mb-4 shadow-none">
-        <CardHeader className="space-y-1">
-          <div className="flex items-center justify-between">
-            <CardTitle>Blocks</CardTitle>
-            <div className="rounded-md bg-primary p-1">
-              <Blocks className="size-5 text-primary-foreground" />
-            </div>
-          </div>
-          <CardDescription>
-            Pre-built starter kits for consistent, repeatable generations
-          </CardDescription>
-        </CardHeader>
-
-        <CardContent>
-          <div className="space-y-2">
-            {blockItems.map((item) => (
-              <div
-                key={item.name}
-                className="flex items-center justify-between"
-              >
-                <Link
-                  href={`/registry/${item.name}`}
-                  className="text-sm hover:underline"
-                >
-                  {item.title}
-                </Link>
-                <ArrowRight className="size-4 text-muted-foreground" />
-              </div>
-            ))}
-          </div>
-        </CardContent>
-      </Card>
-
-      <div className="mb-4 grid gap-6 md:grid-cols-2">
-        <Card className="shadow-none">
-          <CardHeader className="space-y-1">
-            <div className="flex items-center justify-between">
-              <CardTitle>Components</CardTitle>
-              <div className="rounded-md bg-foreground p-1">
-                <ToyBrick className="size-5 text-primary-foreground" />
-              </div>
-            </div>
-            <CardDescription>
-              Reusable UI primitives to build your components
-            </CardDescription>
-          </CardHeader>
-
-          <CardContent>
-            <div className="space-y-2">
-              {componentItems.map((item) => (
-                <div
-                  key={item.name}
-                  className="flex items-center justify-between"
-                >
-                  <Link
-                    href={`/registry/${item.name}`}
-                    className="text-sm hover:underline"
-                  >
-                    {item.title}
-                  </Link>
-                  <ArrowRight className="size-4 text-muted-foreground" />
-                </div>
-              ))}
-            </div>
-          </CardContent>
-        </Card>
-      </div>
-
-      <div className="mb-4 rounded-lg border bg-card p-6">
-        <div className="flex flex-col gap-2">
-          <h2 className="font-semibold text-xl">MCP</h2>
-          <p className="mb-4 text-muted-foreground">
-            Integrate this registry with AI IDEs using Model Context Protocol
-            (MCP) using the following configuration. This utilizes this
-            Registry's theme tokens and CSS variables with the Shadcn CLI. To
-            ensure this works, double check that the{" "}
-            <Link href="/r/registry.json">
-              <code className="inline text-sm tabular-nums underline">
-                style:theme
-              </code>
-            </Link>{" "}
-            contains the same colors as your{" "}
-            <code className="inline text-sm tabular-nums">tokens.css</code>
+      <div className="px-6">
+        <div className="flex flex-col space-y-2 mt-4 p-5 md:mt-2 md:p-10">
+          <h1 className="font-bold text-3xl tracking-tight md:text-4xl">
+            Add registry
+          </h1>
+          <p>
+            Start by adding all Blok components from the registry to your
+            codebase.
           </p>
 
-          <MCPTabs rootUrl={process.env.VERCEL_PROJECT_PRODUCTION_URL ?? ""} />
+          <Codeblocks code={InstallationCode} showLineNumbers={false} />
         </div>
-      </div>
-
-      <div className="rounded-lg border bg-card p-6">
-        <div className="flex flex-col gap-2">
-          <h2 className="font-semibold text-xl">About</h2>
-          <p className="text-muted-foreground">
-            This registry serves as a central repository for all UI components
-            and blocks used in your applications. It helps maintain consistency
-            across your products and speeds up development by providing
-            ready-to-use components. Each component and block is documented with
-            examples. You can browse components by category, search for specific
-            components, and view examples of how they are used in different
-            contexts.
+        <div className="flex flex-col space-y-2 mt-4 p-5 md:mt-2 md:px-10">
+          <h1 className="font-bold text-3xl tracking-tight md:text-4xl">
+            Add component to page
+          </h1>
+          <p>
+            The command above will add the components to your project. You can
+            then import component onto pages like this.
           </p>
-          <p className="mt-2 text-muted-foreground">
-            To get begin, select a{" "}
-            <a href="/starters" className="underline">
-              starter
-            </a>{" "}
-            and click the <span className="font-bold">Open in v0</span> button.
-            You can also open individual components or blocks in v0 if you want
-            a smaller or more specific starting point.
-          </p>
-
-          <p className="mt-4 text-[#1B1F23] dark:text-white">
-            <a href="https://github.com/vercel/registry-starter">
-              <svg
-                viewBox="0 0 1024 1024"
-                xmlns="http://www.w3.org/2000/svg"
-                role="graphics-symbol"
-                className="mr-1 inline size-4 fill-[#1B1F23] dark:fill-white"
-              >
-                <path
-                  fillRule="evenodd"
-                  clipRule="evenodd"
-                  d="M8 0C3.58 0 0 3.58 0 8C0 11.54 2.29 14.53 5.47 15.59C5.87 15.66 6.02 15.42 6.02 15.21C6.02 15.02 6.01 14.39 6.01 13.72C4 14.09 3.48 13.23 3.32 12.78C3.23 12.55 2.84 11.84 2.5 11.65C2.22 11.5 1.82 11.13 2.49 11.12C3.12 11.11 3.57 11.7 3.72 11.94C4.44 13.15 5.59 12.81 6.05 12.6C6.12 12.08 6.33 11.73 6.56 11.53C4.78 11.33 2.92 10.64 2.92 7.58C2.92 6.71 3.23 5.99 3.74 5.43C3.66 5.23 3.38 4.41 3.82 3.31C3.82 3.31 4.49 3.1 6.02 4.13C6.66 3.95 7.34 3.86 8.02 3.86C8.7 3.86 9.38 3.95 10.02 4.13C11.55 3.09 12.22 3.31 12.22 3.31C12.66 4.41 12.38 5.23 12.3 5.43C12.81 5.99 13.12 6.7 13.12 7.58C13.12 10.65 11.25 11.33 9.47 11.53C9.76 11.78 10.01 12.26 10.01 13.01C10.01 14.08 10 14.94 10 15.21C10 15.42 10.15 15.67 10.55 15.59C13.71 14.53 16 11.53 16 8C16 3.58 12.42 0 8 0Z"
-                  transform="scale(64)"
-                />
-              </svg>
-              <span className="underline">GitHub Repository</span>
-            </a>
-          </p>
+          <p className="text-muted-foreground">(example used: button):</p>
+          <Codeblocks code={sampleButtonCode} />
         </div>
       </div>
     </main>
