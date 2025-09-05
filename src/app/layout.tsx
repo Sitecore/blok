ff2935aa4d5228c18538407c8c9f700af8ab5d73
spm_sitecore <spyros.misichronis@sitecore.com>
2025-09-05 13:02:11 +0300

    fix sidebar and meta

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,8 +10,8 @@ import "@/app/globals.css";
 import { Toaster } from "@/components/ui/sonner";
 
 export const metadata: Metadata = {
-  title: "Registry Starter",
-  description: "Starter to help build a Shadcn Registry using Tailwind v4",
+  title: "Blok Registry (Beta)",
+  description: "Blok Registry is a Shadcn Custom Registry ",
   icons: [{ rel: "icon", url: "/favicon.svg", type: "image/svg+xml" }],
 };
 
