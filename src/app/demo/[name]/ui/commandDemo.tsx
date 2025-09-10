301e0665e37b82f88438e91a4970cd25e66c5149
sc-vijith <v7813985@gmail.com>
2025-09-11 00:51:36 +0530

    remove menubar component temporarily from registry.json (#138)
    * chnage alert dialog delete button color
    
    * remove the datepicker
    
    * remove menubar component from the registry.json
    
    * fixed the command component ui issue
    
    ---------
    
    Co-authored-by: vijithan <virv@sitecore.net>

--- src/app/demo/[name]/ui/commandDemo.tsx
+++ src/app/demo/[name]/ui/commandDemo.tsx
@@ -12,13 +12,15 @@ import {
   CommandShortcut,
 } from "@/components/ui/command";
 import {
-  Calculator,
-  Calendar,
-  CreditCard,
-  Settings,
-  Smile,
-  User,
-} from "lucide-react";
+  mdiCalendarBlankOutline,
+  mdiCalculator,
+  mdiCreditCardCheckOutline,
+  mdiCog,
+  mdiEmoticonHappyOutline,
+  mdiAccountOutline,
+} from "@mdi/js";
+import Icon from "@mdi/react";
+
 const CommandDemo = () => {
   const [open, setOpen] = React.useState(false);
 
@@ -49,32 +51,33 @@ const CommandDemo = () => {
               <CommandEmpty>No results found.</CommandEmpty>
               <CommandGroup heading="Suggestions">
                 <CommandItem>
-                  <Calendar />
+                  <Icon path={mdiCalendarBlankOutline} />
                   <span>Calendar</span>
                 </CommandItem>
                 <CommandItem>
-                  <Smile />
+                  <Icon path={mdiEmoticonHappyOutline} />
                   <span>Search Emoji</span>
                 </CommandItem>
                 <CommandItem>
-                  <Calculator />
+                  <Icon path={mdiCalculator} />
                   <span>Calculator</span>
                 </CommandItem>
               </CommandGroup>
               <CommandSeparator />
               <CommandGroup heading="Settings">
                 <CommandItem>
-                  <User />
+                  <Icon path={mdiAccountOutline} />
                   <span>Profile</span>
                   <CommandShortcut>⌘P</CommandShortcut>
                 </CommandItem>
                 <CommandItem>
-                  <CreditCard />
+                  <Icon path={mdiCreditCardCheckOutline} />
                   <span>Billing</span>
                   <CommandShortcut>⌘B</CommandShortcut>
                 </CommandItem>
                 <CommandItem>
-                  <Settings />
+                  <Icon path={mdiCog} />
+
                   <span>Settings</span>
                   <CommandShortcut>⌘S</CommandShortcut>
                 </CommandItem>
