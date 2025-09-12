1917d18dc9f90430d0c61370bb44a414ac9a9671
sc-ishankalakshan <sc.ishanka@gmail.com>
2025-09-12 14:54:50 +0530

    Styles changes

--- src/app/(registry)/page.tsx
+++ src/app/(registry)/page.tsx
@@ -65,29 +65,27 @@ export default function Home() {
           <Codeblocks code={sampleButtonCode} />
 
           <div className="pt-8">
-            <div className="mb-4 rounded-lg border bg-card p-6">
-              <div className="flex flex-col gap-2">
-                <h2 className="font-semibold text-xl">MCP</h2>
-                <p className="mb-4 text-muted-foreground">
-                  Integrate this registry with AI IDEs using Model Context
-                  Protocol (MCP) using the following configuration. This
-                  utilizes this Registry's theme tokens and CSS variables with
-                  the Shadcn CLI. To ensure this works, double check that the{" "}
-                  <Link href="/r/registry.json">
-                    <code className="inline text-sm tabular-nums underline">
-                      style:theme
-                    </code>
-                  </Link>{" "}
-                  contains the same colors as your{" "}
-                  <code className="inline text-sm tabular-nums">
-                    tokens.css
-                  </code>
-                </p>
+            <h1 className="font-bold text-3xl tracking-tight md:text-4xl">
+              MCP
+            </h1>
+            <p className="pt-2">
+              Integrate this registry with AI IDEs using Model Context Protocol
+              (MCP) using the following configuration. This utilizes this
+              Registry's theme tokens and CSS variables with the Shadcn CLI. To
+              ensure this works, double check that the{" "}
+              <Link href="/r/registry.json">
+                <code className="inline text-sm tabular-nums underline">
+                  style:theme
+                </code>
+              </Link>{" "}
+              contains the same colors as your{" "}
+              <code className="inline text-sm tabular-nums">tokens.css</code>
+            </p>
 
-                <MCPTabs
-                  rootUrl={process.env.VERCEL_PROJECT_PRODUCTION_URL ?? ""}
-                />
-              </div>
+            <div className="pt-2">
+              <MCPTabs
+                rootUrl={process.env.VERCEL_PROJECT_PRODUCTION_URL ?? ""}
+              />
             </div>
           </div>
         </div>
