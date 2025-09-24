4c7383ab1fd3b33f56f1fe6bff64aed5a1112c1c
sc-ishankalakshan <sc.ishanka@gmail.com>
2025-09-24 11:11:36 +0530

    Set bottom positioned tooltip as default and deleted other options

--- src/app/demo/[name]/ui/tooltip.tsx
+++ src/app/demo/[name]/ui/tooltip.tsx
@@ -11,36 +11,12 @@ export const tooltip = {
   components: {
     side: (
       <TooltipProvider>
-        <div className="flex mt-10 gap-4">
-          {/* Top Tooltip */}
+        <div className="flex gap-4">
           <Tooltip>
             <TooltipTrigger asChild>
-              <Button variant="outline">Hover Top</Button>
+              <Button variant="outline">Hover</Button>
             </TooltipTrigger>
-            <TooltipContent side="top">Add to library (Top)</TooltipContent>
-          </Tooltip>
-          {/* Bottom Tooltip */}
-          <Tooltip>
-            <TooltipTrigger asChild>
-              <Button variant="outline">Hover Bottom</Button>
-            </TooltipTrigger>
-            <TooltipContent side="bottom">
-              Add to library (Bottom)
-            </TooltipContent>
-          </Tooltip>
-          {/* Right Tooltip */}
-          <Tooltip>
-            <TooltipTrigger asChild>
-              <Button variant="outline">Hover Right</Button>
-            </TooltipTrigger>
-            <TooltipContent side="right">Add to library (Right)</TooltipContent>
-          </Tooltip>
-          {/* Left Tooltip */}
-          <Tooltip>
-            <TooltipTrigger asChild>
-              <Button variant="outline">Hover Left</Button>
-            </TooltipTrigger>
-            <TooltipContent side="left">Add to library (Left)</TooltipContent>
+            <TooltipContent side="bottom">Add to library</TooltipContent>
           </Tooltip>
         </div>
       </TooltipProvider>
