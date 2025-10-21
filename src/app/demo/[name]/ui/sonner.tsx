0bcf8c6f79ff39b5b3098ef6fc0eff865612653d
Muthula <muhtula.dev@gmail.com>
2025-10-21 14:54:45 +0530

    Removed custom sonner

--- src/app/demo/[name]/ui/sonner.tsx
+++ src/app/demo/[name]/ui/sonner.tsx
@@ -16,7 +16,7 @@ export const sonner = {
     Warning: <WarningSonner />,
     Error: <ErrorSonner />,
     Action: <ActionSonner />,
-    Custom: <CustomToastSonner />,
+    // Custom: <CustomToastSonner />,
     Closable: <ClosableSonner />,
   },
 };
