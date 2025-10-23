ce47adaa158cf241d7404ceede18e853362b11ba
Vishath Amarasinghe - Sitecore SE <vishathamarasinghe@gmail.com>
2025-10-23 14:40:16 +0530

    fix: Accessibility issue of Autocomplete attribute must have a valid value in input and tabs component fixed (#247)
    * input and tabs component auto complete issue fixed
    
    * aria-labels added
    
    ---------
    
    Co-authored-by: Vishath Amarasinghe <vishath.amarasinghe@igt1sl.com>

--- src/app/demo/[name]/ui/input.tsx
+++ src/app/demo/[name]/ui/input.tsx
@@ -7,31 +7,31 @@ export const input = {
     Email: (
       <div className="grid gap-2 w-75 ml-2 mt-2">
         <Label htmlFor="email-input">Email</Label>
-        <Input type="email" id="email-input" placeholder="Enter your email" />
+        <Input type="email" id="email-input" placeholder="Enter your email" autoComplete="email" />
       </div>
     ),
     Text: (
       <div className="grid gap-2 w-75 ml-2">
         <Label htmlFor="text-input">Text Input</Label>
-        <Input type="text" id="text-input" placeholder="Enter text" aria-invalid="true" />
+        <Input type="text" id="text-input" placeholder="Enter text" aria-invalid="true" autoComplete="off" />
       </div>
     ),
     Password: (
       <div className="grid gap-2 w-75 ml-2">
         <Label htmlFor="password-input">Password</Label>
-        <Input type="password" id="password-input" placeholder="Enter your password" />
+        <Input type="password" id="password-input" placeholder="Enter your password" autoComplete="current-password" />
       </div>
     ),
     File: (
       <div className="grid gap-2 w-75 ml-2">
         <Label htmlFor="file-input">File Upload</Label>
-        <Input type="file" id="file-input" />
+        <Input type="file" id="file-input" autoComplete="off" />
       </div>
     ),
     Disabled: (
       <div className="grid gap-2 w-75 ml-2">
         <Label htmlFor="disabled-input">Disabled Input</Label>
-        <Input disabled id="disabled-input" placeholder="This input is disabled" />
+        <Input disabled id="disabled-input" placeholder="This input is disabled" autoComplete="off" />
       </div>
     ),
   },
