db221377bdfb18ed6f7f3319d1c16dc376d2b785
Muthula <muhtula.dev@gmail.com>
2025-11-11 09:41:11 +0530

    Update the code format to align the code extracting logic

--- src/app/demo/[name]/ui/calendar.tsx
+++ src/app/demo/[name]/ui/calendar.tsx
@@ -4,8 +4,14 @@ import { SingleCalendar } from "@/app/demo/[name]/ui/single-calendar";
 export const calendar = {
   name: "calendar",
   components: {
-    Default: <SingleCalendar />,
-    TwoMonths: <MultiCalendar numberOfMonths={2} />,
-    ThreeMonths: <MultiCalendar numberOfMonths={3} />,
+    Default: (
+      <SingleCalendar />
+    ),
+    TwoMonths: (
+      <MultiCalendar numberOfMonths={2} />
+    ),
+    ThreeMonths: (
+      <MultiCalendar numberOfMonths={3} />
+    ),
   },
 };
