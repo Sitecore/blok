9beea8484221c96b5c56f5e83e8954834c8fb6b4
Vishath Amarasinghe <vishath.amarasinghe@igt1sl.com>
2025-11-11 15:18:14 +0530

    removed custom animation

--- src/app/demo/[name]/ui/draggable.tsx
+++ src/app/demo/[name]/ui/draggable.tsx
@@ -1,15 +1,12 @@
 import { BasicDragDrop } from "./draggable-basic";
 import { SortableListDemo } from "./draggable-sortable-list";
 import { CustomHandleDemo } from "./draggable-custom-handle";
-import { WithAnimationsDemo } from "./draggable-with-animations";
 
 export const draggable = {
   name: "draggable",
   components: {
     "Basic Drag and Drop": <BasicDragDrop />,
     "Sortable List": <SortableListDemo />,
-    "Custom Handle": <CustomHandleDemo />,
-    "With Animations": <WithAnimationsDemo />,
+    "Custom Handle": <CustomHandleDemo />
   },
 };
-
