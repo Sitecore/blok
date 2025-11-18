56038958783c5706be669ff03392fc752a354f64
Muthula <muhtula.dev@gmail.com>
2025-11-18 11:09:36 +0530

    Update chart with updated details

--- src/app/demo/[name]/ui/chart.tsx
+++ src/app/demo/[name]/ui/chart.tsx
@@ -10,8 +10,8 @@ export const chart = {
     <AreaChartComponent />
   ),
   usage: [
-    `import { AreaChartComponent } from "@/app/demo/[name]/ui/area-chart"`,
-    `<AreaChartComponent />`
+    `import {\n  Area,\n  AreaChart,\n  CartesianGrid,\n  XAxis,\n} from "recharts"; \nimport {\n  ChartConfig,\n  ChartContainer,\n  ChartTooltip,\n  ChartTooltipContent,\n} from "@/components/ui/chart";`,
+    `<AreaChart>\n  <Area dataKey="desktop" type="natural" fill="var(--color-desktop)" fillOpacity={0.4} stroke="var(--color-desktop)" />\n</AreaChart>`,
   ],
   components: {
     "Area Chart": <AreaChartComponent />,
