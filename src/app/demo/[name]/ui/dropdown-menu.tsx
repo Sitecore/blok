4942f635818e04dae7b2996cfb0f750db5fa4178
Muthula <muhtula.dev@gmail.com>
2025-11-18 11:40:23 +0530

    Update dropdown with updated details

--- src/app/demo/[name]/ui/dropdown-menu.tsx
+++ src/app/demo/[name]/ui/dropdown-menu.tsx
@@ -9,12 +9,18 @@ import {
 
 export const dropdownMenu = {
   name: "dropdown-menu",
+  defaultComponent: (
+    <DropdownMenuSimple />
+  ),
+  usage: [
+    `import {\n  DropdownMenu,\n  DropdownMenuContent,\n  DropdownMenuItem,\n  DropdownMenuLabel,\n  DropdownMenuSeparator,\n  DropdownMenuTrigger,\n} from "@/components/ui/dropdown-menu";`,
+    `<DropdownMenu>\n  <DropdownMenuTrigger>Open</DropdownMenuTrigger>\n  <DropdownMenuContent>\n    <DropdownMenuLabel>My Account</DropdownMenuLabel>\n    <DropdownMenuSeparator />\n    <DropdownMenuItem>Profile</DropdownMenuItem>\n    <DropdownMenuItem>Billing</DropdownMenuItem>\n    <DropdownMenuItem>Team</DropdownMenuItem>\n    <DropdownMenuItem>Subscription</DropdownMenuItem>\n  </DropdownMenuContent>\n</DropdownMenu>`,
+  ],
   components: {
-    Default: <DropdownMenuSimple />,
-    DropdownMenuCheckboxes: <DropdownMenuCheckboxes />,
-    DropdownMenuRadioGroupDemo: <DropdownMenuRadioGroupDemo />,
-    DropdownMenuWithAvatar: <DropdownMenuWithAvatar />,
-    DropdownMenuAvatarOnly: <DropdownMenuAvatarOnly />,
-    DropdownMenuIconColor: <DropdownMenuIconColor />,
+    "Dropdown Menu Checkboxes": <DropdownMenuCheckboxes />,
+    "Dropdown Menu Radio Group Demo": <DropdownMenuRadioGroupDemo />,
+    "Dropdown Menu With Avatar": <DropdownMenuWithAvatar />,
+    "Dropdown Menu Avatar Only": <DropdownMenuAvatarOnly />,
+    "Dropdown Menu Icon Color": <DropdownMenuIconColor />,
   },
 };
