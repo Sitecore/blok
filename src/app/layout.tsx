0c533d330272d50ded999f89cb05f97d8614c12a
vijithan <virv@sitecore.net>
2025-11-23 17:12:49 +0530

    fixed the folder structure of documentation pages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,9 +15,9 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: {
+}: Readonly<{
   children: ReactNode;
-}) {
+}>) {
   return (
     <html lang="en" suppressHydrationWarning>
       <meta
