50c38d701709f100771cbcf7af8f7d5ac48d035b
Vishath Amarasinghe <vishath.amarasinghe@igt1sl.com>
2026-02-09 12:07:08 +0530

    file updated with comment

--- src/app/(registry)/page.tsx
+++ src/app/(registry)/page.tsx
@@ -17,6 +17,7 @@ import { mdiOpenInNew } from "@mdi/js";
 import Icon from "@mdi/react";
 import Link from "next/link";
 
+// Home page for the registry.
 export default function Home() {
   return (
     <main className="w-full bg-subtle-bg">
