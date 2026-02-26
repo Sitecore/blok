9d789552e43790bcb8c96693790f5635a03ea0fd
sc-SitharaBalasuriya <shbr@sitecore.net>
2026-02-26 16:23:15 +0530

    Update Progress Demo.

--- components-demo/progress-demo.tsx
+++ components-demo/progress-demo.tsx
@@ -5,10 +5,11 @@ export function ProgressDemo() {
     <div>
       <h2 className="font-semibold text-4xl wrap-break-words">Progress</h2>
       <div className="min-w-lg">
-        <div className="flex items-center justify-center rounded-t-md p-10">
-          <Progress value={80} />
-        </div>
+      <div className="flex flex-col items-center space-y-10 justify-center rounded-t-md p-10">
+        <Progress variant="default" value={80} />
+        <Progress variant="indeterminate" />
       </div>
+    </div>
     </div>
   );
 }
