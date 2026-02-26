a2063423532f2da5dcd6bada4e0f8c18c30e0df0
sc-SitharaBalasuriya <shbr@sitecore.net>
2026-02-26 16:24:26 +0530

    Updated Progress test file.

--- tests/test-Components/test-Progress.ts
+++ tests/test-Components/test-Progress.ts
@@ -1,23 +1,42 @@
 import { test, expect, Page } from '@playwright/test';
 
 export async function testProgress(page: Page){
-    // Verify thatdisplay progress component
-    const progress = page.locator('[data-slot="progress"]').first();
-    await expect(progress).toBeVisible();
+    // Verify that the Default progress component is visible
+    const progressDefault = page.locator('[data-slot="progress"][aria-label="Progress: 80%"]');
+    await expect(progressDefault).toBeVisible();
 
-    // Check that progress has expected classes
-    const classAttribute = await progress.getAttribute('class');
-    expect(classAttribute).toBeTruthy();
-    expect(classAttribute).toContain('relative');
-    expect(classAttribute).toContain('h-2');
-    expect(classAttribute).toContain('w-full');
-    expect(classAttribute).toContain('overflow-hidden');
-    expect(classAttribute).toContain('rounded-full');
-    expect(classAttribute).toContain('bg-primary/20');
+    // Verify that the Default Progress has the expected attributes
+    await expect(progressDefault).toHaveAttribute('aria-valuemin', '0');
+    await expect(progressDefault).toHaveAttribute('aria-valuemax', '100');
+    await expect(progressDefault).toHaveAttribute('data-max', '100');
+    await expect(progressDefault).toHaveAttribute('class', 'relative h-2 w-full overflow-hidden rounded-full bg-neutral-bg');
 
-    const maxProgress = await progress.getAttribute('data-max');
-    expect(maxProgress).toBe('100');
+    // Verify that the Default Progress indicator has the expected class attributes
+    const defaultIndicatorClass = await progressDefault.locator('[data-slot="progress-indicator"]').getAttribute('class');
+    expect(defaultIndicatorClass).toContain('h-full');
+    expect(defaultIndicatorClass).toContain('rounded-full');
+    expect(defaultIndicatorClass).toContain('transition-all');
+    expect(defaultIndicatorClass).toContain('flex-1');
+    expect(defaultIndicatorClass).toContain('w-full');
+    expect(defaultIndicatorClass).toContain('bg-primary');
 
-    const progressValue = await progress.getAttribute('aria-label');
-    expect(progressValue).toBe('Progress: 80%');
+    // Verify that the Indeterminate progress component is visible
+    const progressIndeterminate = page.locator('[data-slot="progress"][aria-label="Progress"]');
+    await expect(progressIndeterminate).toBeVisible();
+
+    // Verify that the Indeterminate Progress has the expected attributes
+    await expect(progressIndeterminate).toHaveAttribute('aria-valuemin', '0');
+    await expect(progressIndeterminate).toHaveAttribute('aria-valuemax', '100');
+    await expect(progressIndeterminate).toHaveAttribute('data-max', '100');
+    await expect(progressIndeterminate).toHaveAttribute('class', 'relative h-2 w-full overflow-hidden rounded-full bg-neutral-bg');
+
+    // Verify that the Indeterminate Progress indicator has the expected class attributes
+    const IndeterminateIndicatorClass = await progressIndeterminate.locator('[data-slot="progress-indicator"]').getAttribute('class');
+    expect(IndeterminateIndicatorClass).toContain('h-full');
+    expect(IndeterminateIndicatorClass).toContain('rounded-full');
+    expect(IndeterminateIndicatorClass).toContain('transition-all');
+    expect(IndeterminateIndicatorClass).toContain('absolute');
+    expect(IndeterminateIndicatorClass).toContain('min-w-[50%]');
+    expect(IndeterminateIndicatorClass).toContain('w-[50%]');
+    expect(IndeterminateIndicatorClass).toContain('bg-primary');
 }
