fa0ccc5b460012b22550738174cb0a822181abb9
sc-SitharaBalasuriya <shbr@sitecore.net>
2026-02-27 11:16:15 +0530

    Updated spinner demo. Removed badge and button spinner.

--- components-demo/spinner-demo.tsx
+++ components-demo/spinner-demo.tsx
@@ -1,6 +1,4 @@
 import { Spinner } from "@/components/ui/spinner";
-import { Button } from "@/components/ui/button";
-import { Badge } from "@/components/ui/badge";
 
 export function SpinnerDemo() {
   return (
@@ -10,8 +8,9 @@ export function SpinnerDemo() {
 
         {/* Default Spinner */}
         <div id="spinner-default">
-          <div className="flex items-center justify-center w-28">
-            <Spinner />
+          <div className="flex items-center justify-center gap-4 w-28">
+            <Spinner variant="default" />
+            <Spinner variant="primary" />
           </div>
         </div>
 
@@ -22,42 +21,6 @@ export function SpinnerDemo() {
             <Spinner className="size-4" />
             <Spinner className="size-6" />
             <Spinner className="size-8" />
-          </div>
-        </div>
-
-        {/* Button Spinner */} 
-        <div id="spinner-button">
-          <div className="flex flex-col items-center gap-4">
-            <Button disabled size="sm">
-              <Spinner />
-              Loading...
-            </Button>
-            <Button variant="outline" disabled size="sm">
-              <Spinner />
-              Please wait
-            </Button>
-            <Button variant="ghost" disabled size="sm">
-              <Spinner />
-              Processing
-            </Button>
-          </div>
-        </div>
-
-        {/* Badge Spinner */}
-        <div id="spinner-badge">
-          <div className="flex items-center gap-4">
-            <Badge>
-              <Spinner />
-              Syncing
-            </Badge>
-            <Badge variant="bold" colorScheme="primary">
-              <Spinner />
-              Updating
-            </Badge>
-            <Badge variant="bold" colorScheme="success">
-              <Spinner />
-              Processing
-            </Badge>
           </div>
         </div>
 
