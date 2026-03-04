5b087e4ceb6c40bb9ac87a7c0f275582d08194ab
sc-SitharaBalasuriya <shbr@sitecore.net>
2026-03-04 14:02:45 +0530

    Updated label demo code.

--- components-demo/label-demo.tsx
+++ components-demo/label-demo.tsx
@@ -6,10 +6,13 @@ export function LabelDemo() {
     <div>
       <h2 className="font-semibold text-4xl wrap-break-words">Label</h2>
       
-      <div className="flex items-center space-x-2">
-        <Checkbox id="terms" aria-label="Accept terms and conditions" />
-        <Label htmlFor="terms">Accept terms and conditions</Label>
+      <div id="label-default">
+        <div className="flex items-center space-x-2 p-2">
+          <Checkbox id="terms" aria-label="Accept terms and conditions" />
+          <Label htmlFor="terms">Accept terms and conditions</Label>
+        </div>
       </div>
+
     </div>
   );
 }
