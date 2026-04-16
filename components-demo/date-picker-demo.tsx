4b1330779d86c444184845bd20f68e886f62c1c0
Sithara Balasuriya <sithara.balasuriya+sitecore@sitecore.com>
2026-04-16 16:49:00 +0530

    Update date picker demo.

--- components-demo/date-picker-demo.tsx
+++ components-demo/date-picker-demo.tsx
@@ -1,8 +1,13 @@
+"use client";
 import { DatePickerSimple } from "@/components/ui/date-picker";
 import { DatePickerWithRange } from "@/components/ui/date-picker";
+import { addDays } from "date-fns";
+import * as React from "react";
 
 export function DatePickerDemo() {
   
+  const [date, setDate] = React.useState<Date | undefined>(() => new Date());
+  const defaultFrom = new Date(new Date().getFullYear(), 0, 20);
 
   return (
     <div>
@@ -10,15 +15,26 @@ export function DatePickerDemo() {
 
     <div className="flex w-full max-w-xl gap-4">
       {/* Simple Date Picker */}
-      <div id="date-picker-simple">
-        <DatePickerSimple />
+        <div id="date-picker-simple">
+          <DatePickerSimple
+            value={date}
+            onChange={setDate}
+            placeholder="Pick a date"
+          />
       </div>
 
       {/* Date Range Picker */}
       <div id="date-picker-range">
-        <DatePickerWithRange />
+        <DatePickerWithRange
+          defaultValue={{
+            from: defaultFrom,
+            to: addDays(defaultFrom, 20),
+          }}
+          placeholder="Pick a date range"
+        />
       </div>
-    </div>  
+    </div>
+
     </div>
   );
 }
