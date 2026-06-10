45a3543a8ecdf61ed8d60d2edbdcea10e6c9ec0c
Sithara Balasuriya <sithara.balasuriya+sitecore@sitecore.com>
2026-06-10 09:21:46 +0530

    Updated tooltip demo and added tooltip provider to layout.tsx.

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,6 +29,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        
         <TooltipProvider>
           <main>{children}</main>
         </TooltipProvider>
--- components-demo/tooltip-demo.tsx
+++ components-demo/tooltip-demo.tsx
@@ -9,14 +9,15 @@ export function TooltipDemo() {
   return (
     <div id="tooltip">
       <h2 className="font-semibold text-4xl wrap-break-words">Tooltip</h2>
+
       <div className="flex gap-4">
-      <Tooltip>
-        <TooltipTrigger asChild>
-          <Button variant="outline">Hover</Button>
-        </TooltipTrigger>
-        <TooltipContent side="bottom">Add to library</TooltipContent>
-      </Tooltip>
-    </div>
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <Button variant="outline">Hover</Button>
+          </TooltipTrigger>
+          <TooltipContent side="bottom">Add to library</TooltipContent>
+        </Tooltip>
+      </div>
     
     </div>
   );
