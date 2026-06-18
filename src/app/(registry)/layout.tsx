51d7d16a26261dce9a286c7ed216bf9905c3a1b4
sc-ArshadHannan <arsh@sitecore.net>
2026-06-18 09:59:56 +0530

    Landmarks should have a unique role or role/label/title (i.e. accessible name) combination

--- src/app/(registry)/layout.tsx
+++ src/app/(registry)/layout.tsx
@@ -25,12 +25,13 @@ export default function RegistryLayout({
     <GainsightProvider>
       <PageViewTracker />
       <div className="flex min-h-screen flex-col w-full bg-sidebar">
-        <div
+        <header
+          aria-label="Site header"
           className="fixed top-0 left-0 right-0 z-50 border-b bg-background border-border w-full"
           style={{ transform: "translateZ(0)", backfaceVisibility: "hidden" }}
         >
           <TopBar />
-        </div>
+        </header>
 
         <div className="flex flex-1 relative mt-12 bg-sidebar">
           <SidebarProvider
